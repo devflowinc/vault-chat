c4ded9a2058a3377bef05e4cbe3742f8b48d723b
Denzell Ford <fdenzell@gmail.com>
2023-04-18 13:38:59 -0500

    hack: Got the bottom bar working

--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -23,44 +23,45 @@ const Layout = () => {
   ]);
 
   return (
-    <div class="flex h-full flex-col items-center">
-      <For each={messages()}>
-        {(message) => {
-          return (
-            <div
-              classList={{
-                "flex p-4 w-full": true,
-                "bg-neutral-200": message.feedback,
-                "bg-neutral-50": !message.feedback,
-              }}
-            >
-              {message.feedback ? (
-                <img class="h-6 w-6" src="/pfp.png" alt="Profile Picture" />
-              ) : (
-                <BiRegularChat />
-              )}
-              <div class="px-2" />
-              <p class="text-xs text-gray-800"> {message.content} </p>
-              {message.feedback && (
-                <div class="ml-4">
-                  <div class="flex items-center text-gray-500">
-                    <p class="text-xs font-bold text-gray-500">Feedback:</p>
-                    <div class="px-1" />
-                    <FiRefreshCcw />
+    <div class="flex h-full min-h-[90vh] flex-col justify-between">
+      <div class="flex h-full  flex-col items-center">
+        <For each={messages()}>
+          {(message) => {
+            return (
+              <div
+                classList={{
+                  "flex p-4 w-full": true,
+                  "bg-neutral-200": message.feedback != undefined,
+                  "bg-neutral-50": message.feedback == undefined,
+                }}
+              >
+                {message.feedback ? (
+                  <img class="h-6 w-6" src="/pfp.png" alt="Profile Picture" />
+                ) : (
+                  <BiRegularChat />
+                )}
+                <div class="px-2" />
+                <p class="text-xs text-gray-800"> {message.content} </p>
+                {message.feedback && (
+                  <div class="ml-4">
+                    <div class="flex items-center text-gray-500">
+                      <p class="text-xs font-bold text-gray-500">Feedback:</p>
+                      <div class="px-1" />
+                      <FiRefreshCcw />
+                    </div>
+                    <p class="text-xs text-gray-500"> {message.feedback} </p>
                   </div>
-                  <p class="text-xs text-gray-500"> {message.feedback} </p>
-                </div>
-              )}
-            </div>
-          );
-        }}
-      </For>
-      <div class="py-2" />
-      <button class="flex w-fit items-center justify-center space-x-4 rounded-xl bg-gray-200 px-4 py-2">
-        <FiRefreshCcw />
-        <p>Regenerate Response</p>
-      </button>
-      <div class="flex-1" />
+                )}
+              </div>
+            );
+          }}
+        </For>
+        <div class="py-2" />
+        <button class="flex w-fit items-center justify-center space-x-4 rounded-xl bg-gray-200 px-4 py-2">
+          <FiRefreshCcw />
+          <p>Regenerate Response</p>
+        </button>
+      </div>
       <div class="flex w-full space-x-4 px-4">
         <input
           class="h-12 w-full rounded-xl bg-gray-200 p-4 text-gray-400"
