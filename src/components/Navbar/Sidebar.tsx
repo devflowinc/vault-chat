983a872038a94f3a2f00412186456a9a39f4aa0a
Denzell Ford <fdenzell@gmail.com>
2023-04-20 14:07:57 -0500

    feat: Added scroll on overflow

--- src/components/Navbar/Sidebar.tsx
+++ src/components/Navbar/Sidebar.tsx
@@ -38,33 +38,35 @@ export const Sidebar = (props: SidebarProps) => {
 
   return (
     <Show when={props.sidebarOpen()}>
-      <div class="h-screen w-2/12 bg-neutral-50">
+      <div class="h-screen w-2/12 bg-gray-50">
         <div class="flex h-full flex-col">
-          <div class="flex items-center space-x-4 border-b-2 bg-gray-400 px-3 py-1">
+          <div class="flex items-center space-x-4 border-y border-gray-400 bg-gray-200 px-3 py-1">
             <div class="text-3xl">
               <ImStack />
             </div>
             <div>New Topic</div>
           </div>
-          <For each={props.topics()}>
-            {(topic) => (
-              <div class="flex items-center space-x-4 border-b-2 px-3 py-1">
-                <div class="text-3xl">
-                  {topic.resolved ? <AiOutlineCheck /> : <TiTimes />}
+          <div class="overflow-y-scroll">
+            <For each={props.topics()}>
+              {(topic) => (
+                <div class="flex items-center space-x-4 border-y px-3 py-1">
+                  <div class="text-3xl">
+                    {topic.resolved ? <AiOutlineCheck /> : <TiTimes />}
+                  </div>
+                  <div>{topic.name}</div>
                 </div>
-                <div>{topic.name}</div>
-              </div>
-            )}
-          </For>
+              )}
+            </For>
+          </div>
           <div class="flex-1" />
-          <div class="flex items-center space-x-4 border-b-2 bg-gray-400 px-3 py-1">
+          <div class="flex items-center space-x-4 border-y bg-gray-200 px-3 py-1">
             <div class="text-3xl">
               <FiHelpCircle />
             </div>
             <div>Help</div>
           </div>
           <button
-            class="flex items-center space-x-4 rounded-br-md border-b-2 bg-gray-400 px-3 py-1"
+            class="flex items-center space-x-4 rounded-br-md border-y border-gray-400 bg-gray-200 px-3 py-1"
             onClick={() => {
               void fetch(`${api_host}/auth`, {
                 method: "DELETE",
