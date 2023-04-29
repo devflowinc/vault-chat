a31d0e66cb507d0599cba95a4c982d4b2f4e5762
Denzell Ford <fdenzell@gmail.com>
2023-04-29 16:12:11 -0500

    ui: Hero section finished

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,27 @@
 export default function Home() {
   return (
-    <div>
-      <div class="m-4 flex items-center justify-between rounded-2xl bg-white px-4 py-3 shadow-md">
-        <div>
-          <p>
-            <span>Arguflow</span>
+    <div class="bg-neutral-50 dark:bg-neutral-900 dark:text-white">
+      <div class="bg-gradient-radial-t from-fuchsia-300 p-4">
+        <div class="flex items-center justify-between rounded-lg bg-neutral-50 px-4 py-3 shadow-md dark:bg-neutral-800">
+          <div>
+            <p>
+              <span>Arguflow </span>
+              <span class="text-violet-500">AI</span>
+            </p>
+          </div>
+          <div class="rounded-lg bg-cyan-500 px-4 py-2">Start Debating</div>
+        </div>
+        <div class="py-4" />
+        <div class="flex flex-col items-center space-y-8">
+          <p class="text-5xl">
+            <span>Arguflow </span>
             <span class="text-violet-500">AI</span>
           </p>
+          <p>Your personal AI debate coach.</p>
+          <div class="rounded-lg bg-gradient-to-br from-cyan-900 to-cyan-500 px-4 py-2 text-white">
+            Start Debating Now
+          </div>
         </div>
-        <div class="rounded-2xl bg-cyan-500 px-4 py-2">Start Debating</div>
-      </div>
-      <div>
-        <p>
-          <span>Arguflow</span>
-          <span class="text-violet-500">AI</span>
-        </p>
       </div>
     </div>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,14 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-radial-t": "radial-gradient(at top, var(--tw-gradient-stops))",
+        "gradient-radial-b":
+          "radial-gradient(at bottom, var(--tw-gradient-stops))",
+      },
+    },
   },
-  plugins: [
-    require("tailwind-scrollbar")({ nocompatible: true }),
-    require("@tailwindcss/line-clamp"),
-  ]
+  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
 };
