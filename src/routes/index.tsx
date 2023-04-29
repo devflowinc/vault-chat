76e7d6656e69ce68201715409fd1ddc3ff1e624b
Denzell Ford <fdenzell@gmail.com>
2023-04-29 12:53:48 -0500

    ui: Finished Navigation bar for mobile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,21 @@
-export default function Index() {
+export default function Home() {
   return (
     <div>
-      <div>Home Page</div>
+      <div class="m-4 flex items-center justify-between rounded-2xl bg-white px-4 py-3 shadow-md">
+        <div>
+          <p>
+            <span>Arguflow</span>
+            <span class="text-violet-500">AI</span>
+          </p>
+        </div>
+        <div class="rounded-2xl bg-cyan-500 px-4 py-2">Start Debating</div>
+      </div>
+      <div>
+        <p>
+          <span>Arguflow</span>
+          <span class="text-violet-500">AI</span>
+        </p>
+      </div>
     </div>
   );
 }
