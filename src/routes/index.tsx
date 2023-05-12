3b127874929f1884adad613c12d220bb25f8f16f
Nicholas Khami <nicholas.khami@gmail.com>
2023-05-11 22:40:13 -0500

    bugfix: src url for example-dark-desktop-img

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -120,7 +120,7 @@ export default function Home() {
         />
         <img
           class="hidden dark:block"
-          src="/example-darkdesktop.png"
+          src="/example-dark-desktop.png"
           alt=""
           elementtiming={""}
           fetchpriority={"auto"}
