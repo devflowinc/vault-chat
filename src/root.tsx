67b2018d59c49cb20db91f38759913247ef751d6
Nicholas Khami <nicholas.khami@gmail.com>
2023-05-15 09:53:39 -0500

    feature: OG tags

--- src/root.tsx
+++ src/root.tsx
@@ -27,6 +27,37 @@ export default function Root() {
         <Title>AI Debate Coach</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>Arguflow AI Debate Coach</title>
+        <meta
+          name="description"
+          content="Get affordable coaching and instant feedback with 24/7 availability from Arguflow - Your AI Debate coach"
+        />
+
+        <meta property="og:url" content="https://arguflow.com" />
+        <meta property="og:type" content="website/" />
+        <meta property="og:title" content="Arguflow AI Debate Coach" />
+        <meta
+          property="og:description"
+          content="Get affordable coaching and instant feedback with 24/7 availability from Arguflow - Your AI Debate coach"
+        />
+        <meta
+          property="og:image"
+          content="https://blog.arguflow.com/arguflow-og.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="arguflow.com" />
+        <meta property="twitter:url" content="https://arguflow.com" />
+        <meta name="twitter:title" content="Arguflow AI Debate Coach" />
+        <meta
+          name="twitter:description"
+          content="Get affordable coaching and instant feedback with 24/7 availability from Arguflow - Your AI Debate coach"
+        />
+        <meta
+          name="twitter:image"
+          content="https://blog.arguflow.com/arguflow-og.png"
+        />
       </Head>
       <Body>
         <ErrorBoundary>
